46523ea62d4a9a8c31c0d0f993095c1962c08db8
elen <elenndev@gmail.com>
2025-12-24 15:51:16 -0300

    add branch and time to lualine

--- .config/nvim/lua/user/plugins/ui.lua
+++ .config/nvim/lua/user/plugins/ui.lua
@@ -24,7 +24,12 @@ return {
         lualine_c = { { "filename", path = 1 } },
         lualine_x = { "diagnostics", "encoding", "filetype" },
         lualine_y = { "progress" },
-        lualine_z = { "location" },
+				        lualine_z = {
+function ()
+return os.date("%H:%M")
+end,
+				},
+
       },
 
       inactive_sections = {
