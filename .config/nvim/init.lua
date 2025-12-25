e6d6a67ee34e1247e55a84b26174ad0dae658c9b
elen <elenndev@gmail.com>
2025-12-24 23:23:01 -0300

    setup tab and formatter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,12 +3,12 @@
 -- require('user.keymaps')
 
 -- config leader key
-vim.g.mapleader = " "  -- leader = espaço
+vim.g.mapleader = " " -- leader = espaço
 vim.opt.clipboard = "unnamedplus"
 
-
 -- Load plugin manager
-require('user.plugins')
+require("user.options")
+require("user.plugins")
 
 -- Set up plugins with dependencies
 -- require('user.treesitter') -- Set up before LSP for better highlighting
@@ -19,17 +19,15 @@ require('user.plugins')
 
 -- Basic error handling wrapper for module loading
 local function safe_require(module)
-  local success, result = pcall(require, module)
-  if not success then
-    vim.notify("Error loading module '" .. module .. "': " .. result, vim.log.levels.ERROR)
-    return nil
-  end
-  return result
+	local success, result = pcall(require, module)
+	if not success then
+		vim.notify("Error loading module '" .. module .. "': " .. result, vim.log.levels.ERROR)
+		return nil
+	end
+	return result
 end
 
 -- Then use it for loading modules
 -- local treesitter = safe_require("user.plugins.treesitter")
 
 -- require("mason").setup()
-
-
--- .config/nvim/lua/user/plugins/ui.lua
+++ .config/nvim/lua/user/plugins/ui.lua
@@ -1,105 +1,161 @@
 return {
-  -- Lualine (statusline)
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-      require("lualine").setup({
-        options = {
-          theme = "auto",
-          icons_enabled = true,
-          globalstatus = true,
-          component_separators = { left = "", right = "" },
-          section_separators = { left = "", right = "" },
-          disabled_filetypes = {
-            statusline = {},
-            winbar = {},
-          },
-        },
+	{
+		"startup-nvim/startup.nvim",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-file-browser.nvim",
+		},
+		config = function()
+			require("startup").setup()
+		end,
+	},
 
-        sections = {
-          lualine_a = { "mode" },
-          lualine_b = { "branch", "diff" },
-          lualine_c = { { "filename", path = 1 } },
-          lualine_x = { "diagnostics", "encoding", "filetype" },
-          lualine_y = { "progress" },
-          lualine_z = {
-            function()
-              return os.date("%H:%M")
-            end,
-          },
-        },
+	{
+		"mhartington/formatter.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local util = require("formatter.util")
+			local augroup = vim.api.nvim_create_augroup
+			local autocmd = vim.api.nvim_create_autocmd
 
-        inactive_sections = {
-          lualine_a = {},
-          lualine_b = {},
-          lualine_c = { "filename" },
-          lualine_x = { "location" },
-          lualine_y = {},
-          lualine_z = {},
-        },
-      })
-    end,
-  },
+			augroup("__formatter__", { clear = true })
 
-  -- Status column with line numbers, folds, and signs
-  {
-    "luukvbaal/statuscol.nvim",
-    config = function()
-      vim.opt.number = true
-      vim.opt.relativenumber = true
-      vim.opt.cursorline = true  -- highlight current line
+			autocmd("BufWritePost", {
+				group = "__formatter__",
+				command = "FormatWrite",
+			})
 
-      local builtin = require("statuscol.builtin")
-      require("statuscol").setup({
-        relculright = true, -- números relativos à direita
+			require("formatter").setup({
+				logging = true,
+				log_level = vim.log.levels.WARN,
 
-        segments = {
-          -- fold column
-          { text = { builtin.foldfunc }, click = nil },
+				filetype = {
+					lua = {
+						require("formatter.filetypes.lua").stylua,
+					},
 
-          -- diagnostic signs
-          {
-            sign = { namespace = { "diagnostic/signs" }, maxwidth = 2, auto = true },
-            click = nil,
-          },
+					javascript = {
+						require("formatter.filetypes.javascript").prettier,
+					},
 
-          -- line numbers
-          { text = { builtin.lnumfunc }, click = nil },
+					typescript = {
+						require("formatter.filetypes.typescript").prettier,
+					},
 
-          -- other signs (Git, LSP, etc.)
-          {
-            sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
-            click = nil,
-          },
-        },
-      })
-    end,
-  },
+					python = {
+						require("formatter.filetypes.python").black,
+					},
 
-  -- Barbecue - breadcrumbs
+					["*"] = {
+						require("formatter.filetypes.any").remove_trailing_whitespace,
+					},
+				},
+			})
+		end,
+	},
+
+	-- Lualine (statusline)
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "auto",
+					icons_enabled = true,
+					globalstatus = true,
+					component_separators = { left = "", right = "" },
+					section_separators = { left = "", right = "" },
+					disabled_filetypes = {
+						statusline = {},
+						winbar = {},
+					},
+				},
+
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch", "diff" },
+					lualine_c = { { "filename", path = 1 } },
+					lualine_x = { "diagnostics", "encoding", "filetype" },
+					lualine_y = { "progress" },
+					lualine_z = {
+						function()
+							return os.date("%H:%M")
+						end,
+					},
+				},
+
+				inactive_sections = {
+					lualine_a = {},
+					lualine_b = {},
+					lualine_c = { "filename" },
+					lualine_x = { "location" },
+					lualine_y = {},
+					lualine_z = {},
+				},
+			})
+		end,
+	},
+
+	-- Status column with line numbers, folds, and signs
+	{
+		"luukvbaal/statuscol.nvim",
+		config = function()
+			vim.opt.number = true
+			vim.opt.relativenumber = true
+			vim.opt.numberwidth = 4
+			vim.opt.cursorline = true -- highlight current line
+
+			local builtin = require("statuscol.builtin")
+			require("statuscol").setup({
+				relculright = true, -- números relativos à direita
+
+				segments = {
+					-- fold column
+					{ text = { builtin.foldfunc }, click = nil },
+
+					-- diagnostic signs
+					{
+						sign = { namespace = { "diagnostic/signs" }, maxwidth = 2, auto = true },
+						click = nil,
+					},
+
+					-- line numbers
+					{ text = { builtin.lnumfunc }, click = nil, colwidth = 2 },
+
+					-- other signs (Git, LSP, etc.)
+					{
+						sign = { name = { ".*" }, maxwidth = 2, colwidth = 2, auto = true, wrap = true },
+						click = nil,
+					},
+				},
+			})
+		end,
+	},
+
+	-- Barbecue - breadcrumbs
 	-- not working fix later
-  {
-    "utilyre/barbecue.nvim",
-    name = "barbecue",
-    version = "*",
-    dependencies = {
-      "SmiteshP/nvim-navic",
-      "nvim-tree/nvim-web-devicons", -- optional dependency
-    },
-    opts = {
-     -- here your config
-      attach_navic = true, -- connect nvim-navic
-      show_dirname = true,
-      show_basename = true,
-      theme = "auto",
-    },
-  },
+	{
+		"utilyre/barbecue.nvim",
+		name = "barbecue",
+		version = "*",
+		dependencies = {
+			"SmiteshP/nvim-navic",
+			"nvim-tree/nvim-web-devicons", -- optional dependency
+		},
+		opts = {
+			-- here your config
+			attach_navic = true, -- connect nvim-navic
+			show_dirname = true,
+			show_basename = true,
+			theme = "auto",
+		},
+	},
 
-  -- indentation GUIDES
-  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+	-- indentation GUIDES
+	{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
 }
-
