239dd82160ade4f84a39e61fc309d50db644c275
elen <elenndev@gmail.com>
2025-12-26 21:42:53 -0300

    ok now code fold its working tsx, lua and rust finally omfg

--- .config/nvim/lua/user/plugins/editor.lua
+++ .config/nvim/lua/user/plugins/editor.lua
@@ -35,4 +35,42 @@ return {
 			autotag.setup({})
 		end,
 	},
+	{
+		{
+			"kevinhwang91/nvim-ufo",
+			dependencies = {
+				"kevinhwang91/promise-async",
+			},
+			event = "BufReadPost",
+			config = function()
+				-- Opções globais de folding (OBRIGATÓRIAS pro ufo)
+				vim.o.foldcolumn = "1"
+				vim.o.foldlevel = 99
+				vim.o.foldlevelstart = 99
+				vim.o.foldenable = true
+
+				-- Keymaps padrão do ufo
+				vim.keymap.set("n", "zR", function()
+					require("ufo").openAllFolds()
+				end, { desc = "Open all folds" })
+
+				vim.keymap.set("n", "zM", function()
+					require("ufo").closeAllFolds()
+				end, { desc = "Close all folds" })
+
+				-- Setup do ufo (ESTRATÉGIA IDEAL)
+				require("ufo").setup({
+					provider_selector = function(bufnr, filetype, buftype)
+						-- JSX / TSX / Rust: indent é o que funciona
+						if filetype == "typescriptreact" or filetype == "javascriptreact" or filetype == "rust" then
+							return { "indent" }
+						end
+
+						-- resto: treesitter primeiro, indent fallback
+						return { "treesitter", "indent" }
+					end,
+				})
+			end,
+		},
+	},
 }
