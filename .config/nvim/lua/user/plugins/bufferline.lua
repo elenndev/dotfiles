bd8b5bedc6bf5718f32ea08a806c1aa807d8b3b4
elen <elenndev@gmail.com>
2025-12-27 22:04:21 -0300

    update bufferline ui

--- .config/nvim/lua/user/plugins/bufferline.lua
+++ .config/nvim/lua/user/plugins/bufferline.lua
@@ -1,48 +1,52 @@
 return {
-  "akinsho/bufferline.nvim",
-  version = "*",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  event = "VeryLazy",
-  config = function()
-    local ok, bufferline = pcall(require, "bufferline")
-    if not ok then
-      return
-    end
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	event = "VeryLazy",
+	config = function()
+		local ok, bufferline = pcall(require, "bufferline")
+		if not ok then
+			return
+		end
 
-    bufferline.setup({
-      options = {
-        mode = "buffers",
-        separator_style = "slant",
-        always_show_bufferline = false,
-        show_buffer_close_icons = false,
-        show_close_icon = false,
-        color_icons = true,
-      },
-      highlights = {
-        separator = {
-          guifg = "#073642",
-          guibg = "#002b36",
-        },
-        separator_selected = {
-          guifg = "#073642",
-        },
-        background = {
-          guifg = "#657b83",
-          guibg = "#002b36",
-        },
-        buffer_selected = {
-          guifg = "#fdf6e3",
-          gui = "bold",
-        },
-        fill = {
-          guibg = "#073642",
-        },
-      },
-    })
+		bufferline.setup({
+			options = {
+				mode = "buffers",
+				separator_style = "slant",
+				always_show_bufferline = false,
+				show_buffer_close_icons = false,
+				show_close_icon = false,
+				color_icons = true,
+			},
+			highlights = {
+				separator = {
+					guifg = "#30363d",
+					guibg = "#0d1117",
+				},
+				separator_selected = {
+					guifg = "#58a6ff",
+					guibg = "#0d1117",
+				},
+				background = {
+					guifg = "#8b949e",
+					guibg = "#0d1117",
+				},
+				buffer_selected = {
+					guifg = "#c9d1d9",
+					guibg = "#161b22",
+					gui = "bold",
+				},
+				fill = {
+					guibg = "#0d1117",
+				},
+				modified = {
+					fg = "#d29922",
+				},
+			},
+		})
 
-    -- Keymaps
-    vim.keymap.set("n", "<S-l>", "<Cmd>BufferLineCycleNext<CR>", { silent = true })
-    vim.keymap.set("n", "<S-h>", "<Cmd>BufferLineCyclePrev<CR>", { silent = true })
-  end,
+		vim.keymap.set("n", "<S-l>", "<Cmd>BufferLineCycleNext<CR>", { silent = true })
+		vim.keymap.set("n", "<S-h>", "<Cmd>BufferLineCyclePrev<CR>", { silent = true })
+		vim.keymap.set("n", "<leader>b", ":bd<CR>", { silent = true })
+	end,
 }
-
