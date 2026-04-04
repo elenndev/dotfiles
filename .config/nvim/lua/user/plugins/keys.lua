e0fcbd7c771588609bbf3edfe7b544975f71e109
elen <elenndev@gmail.com>
2026-04-03 21:51:32 -0300

    enhance nvim config: keys

--- .config/nvim/lua/user/plugins/keys.lua
+++ .config/nvim/lua/user/plugins/keys.lua
@@ -4,8 +4,8 @@ return {
 		event = "VeryLazy",
 		config = function()
 			require("showkeys").setup({
-				timeout = 1, -- always show
-				maxkeys = 4, -- show last key
+				timeout = 1,
+				maxkeys = 4,
 				show_count = false,
 			})
 
@@ -17,7 +17,7 @@ return {
 
 	{
 		"folke/which-key.nvim",
-		lazy = false,
+		event = "VeryLazy",
 		config = function()
 			local ok, which_key = pcall(require, "which-key")
 			if not ok then
@@ -28,46 +28,34 @@ return {
 				plugins = {
 					marks = true,
 					registers = true,
-					spelling = { enabled = true, suggestions = 20 },
-					presets = {
-						operators = true,
-						motions = true,
-						text_objects = true,
-						windows = true,
-						nav = true,
-						z = true,
-						g = true,
-					},
+					spelling = { enabled = false },
 				},
-				layout = { height = { min = 4, max = 25 }, width = { min = 20, max = 50 } },
-				win = { border = "rounded", padding = { 2, 2, 2, 2 } },
+				win = { border = "rounded" },
 			})
 
-			local mappings = {
-				{ "<leader>b", group = "Buffer" },
-				{ "<leader>bd", desc = "<cmd>bdelete<cr>" },
-				{ "<leader>bn", desc = "<cmd>bnext<cr>" },
-				{ "<leader>bp", desc = "<cmd>bprevious<cr>" },
+			which_key.register({
+				["<leader>b"] = { name = "Buffer" },
+				["<leader>bd"] = { "<cmd>bdelete<cr>", "Delete buffer" },
+				["<leader>bn"] = { "<cmd>bnext<cr>", "Next buffer" },
+				["<leader>bp"] = { "<cmd>bprevious<cr>", "Prev buffer" },
 
-				{ "<leader>e", desc = "<cmd>Neotree toggle<cr>" },
+				["<leader>e"] = { "<cmd>Neotree toggle<cr>", "Explorer" },
 
-				{ "<leader>f", group = "File" },
-				{ "<leader>fb", desc = "<cmd>Telescope buffers<cr>" },
-				{ "<leader>ff", desc = "<cmd>Telescope find_files<cr>" },
-				{ "<leader>fg", desc = "<cmd>Telescope live_grep<cr>" },
-				{ "<leader>fn", desc = "<cmd>enew<cr>" },
-				{ "<leader>fr", desc = "<cmd>Telescope oldfiles<cr>" },
+				["<leader>f"] = { name = "File" },
+				["<leader>fb"] = { "<cmd>Telescope buffers<cr>", "Buffers" },
+				["<leader>ff"] = { "<cmd>Telescope find_files<cr>", "Find files" },
+				["<leader>fg"] = { "<cmd>Telescope live_grep<cr>", "Grep" },
+				["<leader>fn"] = { "<cmd>enew<cr>", "New file" },
+				["<leader>fr"] = { "<cmd>Telescope oldfiles<cr>", "Recent" },
 
-				{ "<leader>l", group = "LSP" },
-				{ "<leader>lR", desc = "<cmd>lua vim.lsp.buf.rename()<cr>" },
-				{ "<leader>la", desc = "<cmd>lua vim.lsp.buf.code_action()<cr>" },
-				{ "<leader>ld", desc = "<cmd>Telescope lsp_definitions<cr>" },
-				{ "<leader>lf", desc = "<cmd>lua vim.lsp.buf.format()<cr>" },
-				{ "<leader>lh", desc = "<cmd>lua vim.lsp.buf.hover()<cr>" },
-				{ "<leader>lr", desc = "<cmd>Telescope lsp_references<cr>" },
-			}
-
-			which_key.register(mappings)
+				["<leader>l"] = { name = "LSP" },
+				["<leader>lR"] = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
+				["<leader>la"] = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code action" },
+				["<leader>ld"] = { "<cmd>Telescope lsp_definitions<cr>", "Definitions" },
+				["<leader>lf"] = { "<cmd>lua vim.lsp.buf.format()<cr>", "Format" },
+				["<leader>lh"] = { "<cmd>lua vim.lsp.buf.hover()<cr>", "Hover" },
+				["<leader>lr"] = { "<cmd>Telescope lsp_references<cr>", "References" },
+			})
 		end,
 	},
 }
