34cc0d342f6ed832fd6261d52c88c7a22d250e05
elen <elenndev@gmail.com>
2026-08-29 18:06:29 -0300

    FIX: rewrite markdown injection tha was breakin when using K

--- .config/nvim/lua/user/plugins/treesitter.lua
+++ .config/nvim/lua/user/plugins/treesitter.lua
@@ -1,43 +1,39 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	priority = 100,
-	config = function()
-		local ok, ts_configs = pcall(require, "nvim-treesitter.configs")
-		if not ok then
-			return
-		end
+  "nvim-treesitter/nvim-treesitter",
+  branch = "master",
+  build = ":TSUpdate",
+  lazy = false,
+  priority = 100,
 
-		ts_configs.setup({
-			ensure_installed = {
-				"lua",
-				"http",
-				"vim",
-				"vimdoc",
-				"javascript",
-				"typescript",
-				"tsx",
-				"python",
-				"rust",
-				"html",
-				"css",
-				"json",
-				"yaml",
-				"toml",
-				"markdown",
-				"bash",
-			},
-			auto_install = false,
-			highlight = { enable = true, additional_vim_regex_highlighting = false },
-			indent = { enable = true },
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<CR>",
-					node_incremental = "<CR>",
-					node_decremental = "<BS>",
-				},
-			},
-		})
-	end,
+  config = function()
+    require("nvim-treesitter").setup()
+
+    vim.treesitter.query.set("markdown", "injections", [[
+      (fenced_code_block
+        (info_string
+          (language) @injection.language)
+        (code_fence_content) @injection.content)
+
+      ((html_block) @injection.content
+        (#set! injection.language "html")
+        (#set! injection.combined)
+        (#set! injection.include-children))
+
+      ((minus_metadata) @injection.content
+        (#set! injection.language "yaml")
+        (#offset! @injection.content 1 0 -1 0)
+        (#set! injection.include-children))
+
+      ((plus_metadata) @injection.content
+        (#set! injection.language "toml")
+        (#offset! @injection.content 1 0 -1 0)
+        (#set! injection.include-children))
+
+      ([
+        (inline)
+        (pipe_table_cell)
+      ] @injection.content
+        (#set! injection.language "markdown_inline"))
+    ]])
+  end,
 }
